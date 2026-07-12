e4111d44eb50b8f54fa6da556223c091cf90571d
crishof <cristianhof@gmail.com>
2026-07-12 07:09:03 -0300

    first commit

---
+++ src/main/java/com/zaphirio/ecommercemicroservices/EcommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.zaphirio.ecommercemicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaphirio/ecommercemicroservices/EcommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaphirio.ecommercemicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
